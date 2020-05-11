96284260712c72f49d2197d046b7747f91fae973
Konrad Wodnicki <56640711+konradw98@users.noreply.github.com>
2020-05-11 16:54:37 +0200

    everything works for adding numbers and 4 answers

--- app/src/main/java/com/example/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/example/firstandroidapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.firstandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -24,15 +27,47 @@ public class MainActivity extends AppCompatActivity {
     Button button2;
     Button button3;
     TextView sumTextView;
+    TextView timerTextView;
+    Button playAgainButton;
 
+    public void playAgain(View view){
+        score=0;
+        numberOfQuestions=0;
+        scoreTextView.setText(Integer.toString(score)+"/"+Integer.toString(numberOfQuestions));
+        timerTextView.setText("30s");
+        newQuestion();
+        resultTextView.setText("");
+        playAgainButton.setVisibility(View.INVISIBLE);
+        new CountDownTimer(5100,1000){
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timerTextView.setText( String.valueOf(millisUntilFinished/1000)+"s");
+
+            }
+
+            @Override
+            public void onFinish() {
+                resultTextView.setText("Done!");
+                resultTextView.setTextColor(Color.parseColor("#ff000000"));
+                playAgainButton.setVisibility(View.VISIBLE);
+            }
+        }.start();
+    }
+
+
+
+    @SuppressLint("ResourceAsColor")
     public void chooseAnswer(View view){
       if( Integer.toString(locationOfCorrectAnswer).equals(view.getTag().toString())){
         resultTextView.setText("Correct!");
-
+        resultTextView.setTextColor(Color.parseColor("#4CAF50"));
         score++;
-      }  else {resultTextView.setText("Wrong!");}
+      }  else {resultTextView.setText("Wrong!");
+          resultTextView.setTextColor(Color.parseColor("#F44336"));}
       numberOfQuestions++;
       scoreTextView.setText(Integer.toString(score)+"/"+Integer.toString(numberOfQuestions));
+      newQuestion();
     }
 
     public void start(View view){
@@ -53,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         locationOfCorrectAnswer=rand.nextInt(4);
-
+        answers.clear();
         for(int i=0;i<4;i++){
             if(i==locationOfCorrectAnswer){
                 answers.add(a+b);
@@ -79,7 +114,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        goButton=findViewById(R.id.goButton);
+
         button0=findViewById(R.id.button0);
         button1=findViewById(R.id.button1);
         button2=findViewById(R.id.button2);
@@ -87,7 +122,10 @@ public class MainActivity extends AppCompatActivity {
         sumTextView= findViewById(R.id.sumTextView);
         resultTextView=findViewById(R.id.resultTextView);
         scoreTextView=findViewById(R.id.scoreTextView);
+        timerTextView=findViewById(R.id.timerTextView);
+        playAgainButton=findViewById(R.id.playAgainButton);
 
       newQuestion();
-    }
-}
+        playAgain(findViewById(R.id.timerTextView));
+
+}}
